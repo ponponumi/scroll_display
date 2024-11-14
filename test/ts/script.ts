934128c60af351965735e0804d25b0931590da1d
ponponumi <ponponumi@ponponumi.com>
2024-11-14 20:03:50 +0900

    リストを生成するように変更

--- test/ts/script.ts
+++ test/ts/script.ts
@@ -1,3 +1,13 @@
 import ScrollDisplay from "../../src/main";
 
 let ScrollDisplayObj = new ScrollDisplay("totop", 20);
+
+let listElem = document.getElementById("list");
+
+if (listElem) {
+    for (let i = 0; i < 50; i++){
+        let itemElem = document.createElement("li");
+        itemElem.textContent = "テキストテキスト";
+        listElem.append(itemElem);
+    }
+}
