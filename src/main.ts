e3b47fd59bf8d17d80c5ef46d67241c3582638d4
ponponumi <ponponumi@ponponumi.com>
2024-11-14 20:00:05 +0900

    イベントを追加するメソッドを作成

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,26 @@ class ScrollDisplay{
 
         this.changePoint = changePoint;
     }
+
+    public eventAdd(pointAbove: (elem: HTMLElement) => void, pointBelow: (elem: HTMLElement) => void) {
+        // イベントを追加する
+        // HTML要素がnullの場合はイベントを追加しない
+        let totop = this.elem;
+        let changePoint = this.changePoint;
+
+        if (totop) {
+            // HTML要素があれば
+            document.addEventListener("scroll", function () {
+                if (window.scrollY > changePoint) {
+                    // 指定したポイントよりも下なら
+                    pointBelow(totop);
+                } else {
+                    // 指定したポイントよりも上なら
+                    pointAbove(totop);
+                }
+            });
+        }
+    }
 }
 
 export default ScrollDisplay;
