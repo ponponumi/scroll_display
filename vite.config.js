08bba9905568158cc463eddc3c7e195442a70fb5
ponponumi <ponponumi@ponponumi.com>
2024-11-14 19:32:11 +0900

    Viteのconfigを作成

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    build: {
+        manifest: true,
+        outDir: './dist',
+        rollupOptions: {
+            input: [
+                'index.html',
+                'test/ts/script.ts',
+            ],
+        },
+    },
+});
