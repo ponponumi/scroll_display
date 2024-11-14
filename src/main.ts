b13e8d4a30a4f5d8c14c16ff5df4c94e3d583b89
ponponumi <ponponumi@ponponumi.com>
2024-11-14 19:39:01 +0900

    スクロールディスプレイ用のクラスを作成

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+class ScrollDisplay{
+    elem: HTMLElement|null = null;
+
+    constructor(idName: string) {
+        let elem = document.getElementById(idName);
+
+        if (elem) {
+            this.elem = elem;
+        }
+    }
+}
